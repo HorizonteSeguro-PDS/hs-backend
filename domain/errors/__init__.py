73a4c6a5c297953ef26f9a07f274ec5254c1a448
Marcelo Falcão <mpf@ic.ufal.br>
2026-06-06 04:45:39 -0300

    feat: :sparkles: Add reusable HTTP error classes

--- domain/errors/__init__.py
+++ domain/errors/__init__.py
@@ -1,1 +1,21 @@
 """Domain errors package."""
+
+from domain.errors.http import (
+    ForbiddenRequestError,
+    GenericError,
+    InternalError,
+    KeyNotFoundError,
+    ResourceAlreadyExists,
+    ResourceInUse,
+    ResourceNotFoundError,
+)
+
+__all__ = [
+    "ForbiddenRequestError",
+    "GenericError",
+    "InternalError",
+    "KeyNotFoundError",
+    "ResourceAlreadyExists",
+    "ResourceInUse",
+    "ResourceNotFoundError",
+]
