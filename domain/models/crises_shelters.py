7a3b57b2ed9a25c38c468498cd4cde27acdda613
Marcelo Falcão <mpf@ic.ufal.br>
2026-06-07 17:10:19 -0300

    feat: :sparkles: Implement CRUD foundation for Crisis and Shelter models with many-to-many relationship

--- domain/models/crises_shelters.py
+++ domain/models/crises_shelters.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.dialects.postgresql import TIMESTAMP, UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -13,8 +13,24 @@ class CrisesShelters(Base):
 
     __tablename__ = "crises_shelters"
 
-    crisis_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
-    shelter_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    crisis_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey(
+            "crises.id",
+            name="fk_crises_shelters_crisis_id_crises",
+            ondelete="CASCADE",
+        ),
+        primary_key=True,
+    )
+    shelter_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey(
+            "shelters.id",
+            name="fk_crises_shelters_shelter_id_shelters",
+            ondelete="CASCADE",
+        ),
+        primary_key=True,
+    )
     joined_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True), server_default=func.now(), nullable=False
     )
